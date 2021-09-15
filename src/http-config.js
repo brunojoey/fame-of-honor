8f3b0af7990f895a572c53bccd8363ade33c938d
Joey Bruno <brunojoey12@gmail.com>
2021-09-15 18:20:00 -0500

    More CORS Testing

--- src/http-config.js
+++ src/http-config.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://fame-of-honor-server.herokuapp.com/api/positions/RB" || "http://localhost:3000/api/",
+  baseURL: "http://localhost:3000/api/",
   headers: {
     "Content-type": "application/json"
-  }
+  },
+  origin: "https://fame-of-honor-server.herokuapp.com/api/"
 });
